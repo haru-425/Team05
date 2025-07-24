9ac31ec8993b94b0838434957151668c930f36d0
yozora911 <puusuke0323113@icloud.com>
2025-07-24 17:23:40 +0900

    メーター

--- ゲームプログラミングⅢ/Source/metar.cpp
+++ ゲームプログラミングⅢ/Source/metar.cpp
@@ -28,6 +28,6 @@ void Metar::render()
 	rc.deviceContext = dc;
 	rc.renderState = graphics.GetRenderState();
 
-	metar->Render(rc, 30, 100, 0, 30, this->gage * 5.0f, 0, 0.1f, 0.1f, 10, 0.5f);
-	frame->Render(rc, 30, 100, 0, 40, 250.0f, 0, 0, 0, 10, 1);
+	metar->Render(rc, 30, 100, 0, 30, this->gage * 5.0f, 0, 0.3f, 0.3f, 1, 0.5f);
+	frame->Render(rc, 30, 100, 0, 40, 250.0f, 0, 0, 0, 1, 1);
 }
