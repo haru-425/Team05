df128a6b5e2111d2038c23b504119e6f90fbedc1
葉月はる <93645335+haru-425@users.noreply.github.com>
2025-08-07 21:43:53 +0900

    修正

--- ゲームプログラミングⅢ/Source/EnemyUI.h
+++ ゲームプログラミングⅢ/Source/EnemyUI.h
@@ -124,11 +124,11 @@ public:
 		for (int i = 0; i < 3; ++i) {
 			ui[i]->Render(
 				rc,
-				(uiPos[i].x - (160 /*- i * 20*/) * scaleX / 2.f),
-				(uiPos[i].y - (160 /*- i * 20*/) * scaleY / 2.f),
+				(uiPos[i].x - (160 / 2 /*- i * 20*/) /** scaleX */ / 2.f),
+				(uiPos[i].y - (160 / 2 /*- i * 20*/) /** scaleY*/ / 2.f),
 				0.0f,
-				(160 /*- i * 20*/) * scaleX,
-				(160 /*- i * 20*/) * scaleY,
+				(160 /*- i * 20*/) / 2 /** scaleX*/,
+				(160 /*- i * 20*/) / 2/* * scaleY*/,
 				0,
 				1.0f, 1.0f, 1.0f, 1.0f
 			);
